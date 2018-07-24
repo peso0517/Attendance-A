df9a4a43fa3734858e613baa591a64c0d5cdf287
peso0517 <peso0517@gmail.com>
2018-07-24 14:26:27 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   
    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'app95066123@heroku.com'
+  host = 'stormy-shore-52598.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
