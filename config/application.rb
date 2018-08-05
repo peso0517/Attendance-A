3a73cb27c8c63300a3ea625aafba1ff9536022cb
peso0517 <peso0517@gmail.com>
2018-08-05 14:50:08 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+     # 認証トークンをremoteフォームに埋め込む
+
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -50,4 +50,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # クッキーを削除してログイン
     log_in_as(@user, remember_me: '0')
   end
+  
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
