6bdd90ae40d8c7ba689a9e65a60da521dd9ea22c
EC2 Default User <ec2-user@ip-172-31-11-132.us-east-2.compute.internal>
2019-01-01 16:10:02 +0000

    Delete-seed-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,3 @@ User.create!(name:  "一般ユーザー",
              basic_work_time: "07:30",
              activated: true,
              activated_at: Time.zone.now)
-
-               
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
-
-# リレーションシップ
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
-
