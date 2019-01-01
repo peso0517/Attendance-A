892fafc73bad5ef7734812a4c9fe21fba8b2a30d
EC2 Default User <ec2-user@ip-172-31-11-132.us-east-2.compute.internal>
2019-01-01 15:56:42 +0000

    Delete-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,22 +27,6 @@ User.create!(name:  "一般ユーザー",
              activated: true,
              activated_at: Time.zone.now)
 
-
-10.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               affiliation: affiliation,
-               specified_work_time: "08:00",
-               basic_work_time: "07:30",
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-               
-  end
                
 users = User.order(:created_at).take(6)
 50.times do
